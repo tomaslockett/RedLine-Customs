9f8b28b052f488954e1aef1405e9eda3ebd6fbbc
tomaslockett <tomaslockett@hotmail.com>
2026-04-20 17:25:10 -0300

    Añadir proyectos BE, BLL, DAL y Servicios a la solución
    Se agregan cuatro nuevos proyectos de biblioteca de clases: RedLine.Be, RedLine.Bll, RedLine.Dal y RedLine.Servicios, cada uno con su configuración inicial (.csproj, AssemblyInfo.cs y Class1.cs). Se actualiza el archivo de solución para incluir estos proyectos.

---
+++ RedLine.Dal/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("RedLine.Dal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RedLine.Dal")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("305ad79c-0a71-4930-98f5-d5d0e8fa2fe3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
