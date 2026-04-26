c488c960b6eee58bdaf05116c47e1624effe0d9a
Agustin Soria <agusor04@hotmail.com>
2026-04-26 16:54:58 -0300

    Añadir clases para gestión de automóviles y mejoras
    Se han creado las clases `AutoBase`, `AutoPersonalizado`, `Cliente`, y `Mejora`, junto con sus implementaciones específicas como `Aleron`, `KitCarroceria`, `Llantas`, `Pintura`, y `Suspension`. Estas clases incluyen propiedades relevantes y constructores para facilitar la inicialización de sus atributos.

---
+++ RedLine.Be/AutoPersonalizacion/Pintura.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Redline.Be
+{
+    public class Pintura : Mejora
+    {
+        public string TipoAcabado { get; set; } // Mate, Satinado, Brillante, Cromado
+        public string CodigoColor { get; set; } // Hexadecimal o código de marca
+        public bool EsVinilo { get; set; } // True si es Wrap, False si es pintura real
+
+        public Pintura() { }
+    }
+}
