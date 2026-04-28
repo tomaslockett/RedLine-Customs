0d8cedfcb53f345cabdc200d6d1c0abfc4149feb
Agustin Soria <agusor04@hotmail.com>
2026-04-28 18:33:01 -0300

    Añadir gestión de eventos y respaldo de base de datos
    Se ha añadido la clase `BLL_Evento` para registrar y listar eventos en la aplicación.
    El archivo `DAL_BackupRestore.cs` ha sido movido a la carpeta `Mappers` y se han añadido nuevos mapeos, incluyendo `DAL_Evento`, que interactúa con la tabla `Bitacora`.
    Además, se ha creado la clase `Evento` en `RedLine.Servicios.csproj` para representar eventos con sus propiedades.
    Se han implementado cambios en la lógica de respaldo y restauración de la base de datos.

---
+++ RedLine.Dal/Mappers/DAL_Evento.cs
@@ -0,0 +1,54 @@
+﻿using RedLine.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedLine.Dal.Mappers
+{
+    public class DAL_Evento : AbstractDAL<int, Evento>
+    {
+        protected override string NombreTabla => "Bitacora";
+        protected override bool RequiereDigitoVerificador => false; 
+
+        protected override string SqlInsertar =>
+            "INSERT INTO Bitacora (Usuario, Fecha, Modulo, Actividad, Criticidad) VALUES (@Usu, @Fec, @Mod, @Act, @Cri)";
+
+        protected override string SqlListar => "SELECT * FROM Bitacora ORDER BY Fecha DESC";
+        protected override string SqlObtenerPorId => "SELECT * FROM Bitacora WHERE ID = @ID";
+
+        protected override string SqlModificar => "";
+        protected override string SqlEliminar => "";
+
+        protected override void ConfigurarParametros(SqlCommand cmd, Evento entidad)
+        {
+            cmd.Parameters.AddWithValue("@Usu", entidad.Usuario);
+            cmd.Parameters.AddWithValue("@Fec", entidad.Fecha);
+            cmd.Parameters.AddWithValue("@Mod", entidad.Modulo);
+            cmd.Parameters.AddWithValue("@Act", entidad.Actividad);
+            cmd.Parameters.AddWithValue("@Cri", entidad.Criticidad);
+        }
+
+        protected override void ConfigurarParametrosId(SqlCommand cmd, int id)
+        {
+            cmd.Parameters.AddWithValue("@ID", id);
+        }
+
+        protected override Evento Mapear(SqlDataReader lector)
+        {
+            return new Evento
+            {
+                ID = Convert.ToInt32(lector["ID"]),
+                Usuario = lector["Usuario"].ToString(),
+                Fecha = Convert.ToDateTime(lector["Fecha"]),
+                Modulo = lector["Modulo"].ToString(),
+                Actividad = lector["Actividad"].ToString(),
+                Criticidad = Convert.ToInt32(lector["Criticidad"])
+            };
+        }
+
+        public override Evento ObtenerPorEntidad(Evento entidad) => null;
+    }
+}
