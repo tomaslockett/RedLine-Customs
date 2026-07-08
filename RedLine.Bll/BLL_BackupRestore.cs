f205015ea6cbd6e685eed2bc32b70458f6149022
Agustin Soria <agusor04@hotmail.com>
2026-07-08 19:56:24 -0300

    Añadir funcionalidad de backup y restauración de DB
    Se ha añadido un nuevo archivo ASP.NET `BackupRestore.aspx` para gestionar copias de seguridad y restauraciones de la base de datos, junto con su código detrás `BackupRestore.aspx.cs` y archivo de diseño `BackupRestore.aspx.designer.cs`.
    
    Se ha creado un archivo CSS `BackupRestore.css` para estilizar la interfaz.
    
    Se han actualizado las consultas SQL en `BLL_BackupRestore.cs` y `DAL_BackupRestore.cs` para reflejar el nuevo nombre de la base de datos `RedLineCustomsDB`.
    
    Además, se ha añadido un bloque `try-catch` en `RealizarRestore` para manejar excepciones y se han realizado cambios en `Web.config` para actualizar la configuración de la aplicación.
    
    Finalmente, se han añadido referencias a los nuevos archivos en el proyecto `RedLine.Web.csproj`.

--- RedLine.Bll/BLL_BackupRestore.cs
+++ RedLine.Bll/BLL_BackupRestore.cs
@@ -1,11 +1,12 @@
 ﻿using RedLine.Dal;
+using RedLine.Servicios;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RedLine.Servicios;
 
 namespace RedLine.Bll
 {
@@ -28,7 +29,7 @@ namespace RedLine.Bll
             string rutaFinal = Path.Combine(backupPath, nombreArchivo);
 
             _dal.RealizarBackup(rutaFinal);
-            string user = SessionManager.Instancia.Usuario.Email; 
+            string user = SessionManager.Instancia.Usuario.Email;
             _bllEvento.Registrar(user, "Base de Datos", $"Copia de seguridad generada con éxito: {nombreArchivo}", 2);
         }
 
@@ -40,8 +41,32 @@ namespace RedLine.Bll
             if (!Path.GetExtension(restorePath).Equals(".bak", StringComparison.OrdinalIgnoreCase))
                 throw new Exception("El archivo seleccionado no tiene un formato válido (.bak).");
 
+            try
+            {
+                string carpetaContenedora = Path.GetDirectoryName(restorePath);
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "icacls",
+                    Arguments = $"\"{carpetaContenedora}\" /grant \"NT SERVICE\\MSSQLSERVER\":(OI)(CI)RX /T",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (Process proceso = Process.Start(psi))
+                {
+                    proceso.WaitForExit();
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
             _dal.RealizarRestore(restorePath);
-            string user = SessionManager.Instancia.Usuario.Email; 
+            string user = SessionManager.Instancia.Usuario.Email;
             string nombreArchivo = Path.GetFileName(restorePath);
             _bllEvento.Registrar(user, "Base de Datos", $"Restauración completa del sistema realizada desde: {nombreArchivo}", 3);
         }
---
+++ RedLine.Web/BackupRestore.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="BackupRestore" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BackupRestore.aspx.cs" Inherits="RedLine.Web.BackupRestore" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <link href="Content/BackupRestore.css" rel="stylesheet" type="text/css" />
+
+    <h1>Gestion de Backup y Restore</h1>
+    <div>
+            <h2>Copia de Seguridad</h2>
+            <asp:TextBox ID="txtRutaBackup" runat="server" Width="350px"></asp:TextBox>
+            <asp:Button ID="btnGenerar" runat="server" Text="Generar copia" OnClick="btnGenerar_Click" />
+
+            <hr />
+
+            <h2>Restaurar Base de Datos</h2>
+            <asp:FileUpload ID="fileUploadRestore" runat="server" />
+            <asp:Button ID="btnRestaurar" runat="server" Text="Restaurar" OnClick="btnRestaurar_Click" />
+
+            <hr />
+            <asp:Label ID="lblEstado" runat="server" Font-Bold="true"></asp:Label>
+        </div>
+</asp:Content>
